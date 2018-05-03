79d450d900bb977fbf90798ef782741e5276fefd
PyVlad <32043790+pyvlad@users.noreply.github.com>
2018-05-03 21:56:07 +0300

    remove sessions, store username in localStorage of client instead

--- application.py
+++ application.py
@@ -1,25 +1,18 @@
 import os
 from datetime import timedelta
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") or 'you-will-never-guess'
-app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=100*365)
 socketio = SocketIO(app)
 
 
 @app.route("/")
 def index():
-    if "username" in session:
-        return render_template("index.html", username=session["username"])
+    return render_template("index.html")
 
-    username = request.args.get("username")
-    if username:
-        session["username"] = username
-        session.permanent = True
-    return render_template("index.html", username=username)
 
 if __name__ == "__main__":
     app.run(debug=True)
