0e8ba31e84fe219f799440606b3cb20b72bc2e44
PyVlad <32043790+pyvlad@users.noreply.github.com>
2018-05-04 18:46:03 +0300

    add basic channels page, with list of current channels on connect and option to create a new channel

--- application.py
+++ application.py
@@ -1,17 +1,57 @@
 import os
+import json
 from datetime import timedelta
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") or 'you-will-never-guess'
 socketio = SocketIO(app)
 
 
+CHANNELS = []
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/channels")
+def channels():
+    return render_template("channels.html")
+
+
+@socketio.on('connect')
+def send_current_channels():
+    emit('all channels', CHANNELS)
+
+
+@socketio.on('create channel')
+def create_channel(data):
+    global CHANNELS
+    channel = data["channel"]
+    if channel not in CHANNELS:
+        CHANNELS += [channel]
+        emit('new channel', channel, broadcast=True)
+        print("new channel {channel} created")
+    else:
+        print(f"channel {channel} already exists")
+
+
+# @socketio.on('join')
+# def on_join(data):
+#     username = data['username']
+#     channel = data['channel']
+#     join_room(channel)
+#     send(username + ' has entered the room.', room=channel)
+#
+# @socketio.on('leave')
+# def on_leave(data):
+#     username = data['username']
+#     channel = data['channel']
+#     leave_room(channel)
+#     send(username + ' has left the room.', room=channel)
 
 
 if __name__ == "__main__":
